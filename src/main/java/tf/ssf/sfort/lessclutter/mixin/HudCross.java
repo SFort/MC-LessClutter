728da5fcf42f6290f9f683dd48a8f21d7212ee90
SFort <github@ssf.tf>
2021-06-03 01:51:16 +0200

    dynamicCrosshair now checks offhand

--- src/main/java/tf/ssf/sfort/lessclutter/mixin/HudCross.java
+++ src/main/java/tf/ssf/sfort/lessclutter/mixin/HudCross.java
@@ -2,6 +2,7 @@ package tf.ssf.sfort.lessclutter.mixin;
 
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,9 +15,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class HudCross {
 	@Shadow
 	private ItemStack currentStack;
+
+	@Shadow
+	protected abstract PlayerEntity getCameraPlayer();
+
 	@Inject(method = "renderCrosshair", at = @At("HEAD"), cancellable = true)
 	private void renderCrosshair(MatrixStack matrices, CallbackInfo info) {
-		if (Config.dynamicCrosshair && !(currentStack.getItem() instanceof BlockItem))
+		if (Config.dynamicCrosshair && !(currentStack.getItem() instanceof BlockItem || this.getCameraPlayer().getOffHandStack().getItem() instanceof BlockItem))
 			info.cancel();
 	}
 }
