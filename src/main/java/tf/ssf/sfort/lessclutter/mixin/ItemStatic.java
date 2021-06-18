e1a48ca6a202830b77d1777bbc4a64ae2186174f
SFort <github@ssf.tf>
2021-06-18 16:41:49 +0200

    fix static tooltips

--- src/main/java/tf/ssf/sfort/lessclutter/mixin/ItemStatic.java
+++ src/main/java/tf/ssf/sfort/lessclutter/mixin/ItemStatic.java
@@ -2,35 +2,37 @@ package tf.ssf.sfort.lessclutter.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.OrderedText;
-import net.minecraft.text.Text;
+import net.minecraft.util.math.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-@Mixin(value = Screen.class, priority = 4642)
+@Mixin(value = Screen.class, priority = 4641)
 public abstract class ItemStatic {
 
 	@Shadow
 	protected MinecraftClient client;
 
-	@Inject(method = "renderOrderedTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V",
+	@Inject(method = "renderTooltipFromComponents(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V",
 			at = @At(value = "HEAD"), cancellable = true)
-	public void renderOrderedTooltip(MatrixStack matrices, List<? extends OrderedText> lines, int x, int y, CallbackInfo info) {
+	public void renderOrderedTooltip(MatrixStack matrices, List<TooltipComponent> lines, int x, int y, CallbackInfo info) {
 		y=10;
+		Matrix4f matrix4f = matrices.peek().getModel();
+		VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
 		for(x=0; x<lines.size(); x++){
-			client.textRenderer.drawWithShadow(matrices, lines.get(x), 10, y, -1);
-			y += client.textRenderer.getWidth(lines.get(x)) == 0 ? 3 : 10;
+			lines.get(x).drawText(client.textRenderer, 10, y, matrix4f, immediate);
+			y += lines.get(x).getWidth(client.textRenderer) == 0 ? 3 : 10;
 		}
+		immediate.draw();
+		matrices.pop();
 		info.cancel();
 	}
 }
