75dcd540d192429801dc5936d5d857034f6abb59
SFort <github@ssf.tf>
2021-10-28 20:17:40 +0200

    fix possible modmenu crash

--- src/main/java/tf/ssf/sfort/lessclutter/ModMenu.java
+++ src/main/java/tf/ssf/sfort/lessclutter/ModMenu.java
@@ -2,9 +2,11 @@ package tf.ssf.sfort.lessclutter;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class ModMenu implements ModMenuApi {
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ConfigScreen::new;
+        if (FabricLoader.getInstance().isModLoaded("fscript")) return ConfigScreen::new;
+        return ModMenuApi.super.getModConfigScreenFactory();
     }
 }
